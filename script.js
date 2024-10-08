332ca243f444f2538cfb138513f6c462af2a0d09
Akash Hans <162800417+AkxshHans@users.noreply.github.com>
2024-10-08 14:50:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+
+
+
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+  
